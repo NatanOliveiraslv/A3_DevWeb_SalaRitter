c41a394c280db1cfe6b3d32c4fc555d608fd1a32
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2023-11-17 22:28:45 -0300

    turmas no painel do professor

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from usuarios.models import Aluno, Professor
+from materia.models import Materia
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
@@ -45,7 +46,8 @@ def painelUsuario(request):
         #verifca para qual pagina o usuário irá ser enviado. Se for aluno para a do aluno
         # se professor para a dor professor.
         if validaProfessor(usuario):
-            return render(request, 'usuarios/painel_professor.html')
+            turma = Materia.objects.all().filter(professor=Professor.objects.get(user=User.objects.get(username=usuario)))
+            return render(request, 'usuarios/painel_professor.html', {'turma':turma})
         elif validaAluno(usuario):
             return render(request, 'usuarios/painel_aluno.html')
         else:
